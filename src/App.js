03dac3a34259ecd8911eb97f7e2d30c740f583b2
sbal13 <sbal13@gmail.com>
2017-09-01 17:49:31 -0400

    Location search finished

--- src/App.js
+++ src/App.js
@@ -2,27 +2,158 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Papa from 'papaparse'
+import Filter from './components/Filter'
+
 
 
 class App extends Component {
 
-  state = {}
-
-
-  test = ()=> {
+  getLocations = () => {
     Papa.parse("http://a841-dotweb01.nyc.gov/datafeeds/ParkingReg/locations.CSV", {
       download: true,
-      complete: function(results) {
-        console.log(results.data[0]);
-      }
+      complete: this.parseLocationData
     });
+  };
+
+  getSigns = () => {
+    Papa.parse("http://a841-dotweb01.nyc.gov/datafeeds/ParkingReg/signs.CSV", {
+      download: true,
+      complete: this.parseSignData
+    });
+  };
+
+  state = {
+    boroughFilter: "",
+    filteredResults: "",
+    temporaryResults: [],
+    mainFilterTemp: "",
+    fromFilterTemp: "",
+    toFilterTemp: "",
+    mainFilter: false,
+    fromFilter: false,
+    toFilter: false,
+    locations: [],
+    signs: []
+  };
+
+  parseLocationData = (results) => {
+    this.setState({
+          locations: results.data.map(row => {
+            return  {
+              borough: row[0],
+              code: row[1],
+              main: row[2],
+              from: row[3],
+              to: row[4],
+              side: row[5]
+            }
+          })
+        })
+  };
+
+  parseSignData = (results) => {
+    this.setState({
+          signs: results.data.map(row => {
+            return  {
+              borough: row[0],
+              code: row[1],
+              sequence: row[2],
+              distance: row[3],
+              arrow: row[4],
+              description: row[5],
+              signCode: row[6]
+            }
+          })
+        })
+  };
+
+
+  changeBorough = (b) => {
+    const withinBor = this.state.locations.filter(location => location.borough === b)
+
+    this.setState({
+      filteredResults: withinBor,
+      boroughFilter: b,
+      mainFilter: false,
+      mainFilterTemp: "",
+      fromFilter: false,
+      fromFilterTemp: "",
+      toFilter: false,
+      toFilterTemp: ""
+    })
+  };
+
+  changeMainFilter = (main) => {
+    var sanitized = main.toUpperCase().trim().replace(/\s/g, "")
+    const filteredByMain = this.state.filteredResults.filter(location =>  location.main.trim().replace(/\s/g, "").includes(sanitized))
+    this.setState({
+      mainFilterTemp: main,
+      temporaryResults: filteredByMain,
+      fromFilter: false,
+      toFilter: false
+    })
+  };
+
+  changeFromFilter = (fromInput) => {
+    var sanitized = fromInput.toUpperCase().trim().replace(/\s/g, "")
+    const filteredByFromInput = this.state.filteredResults.filter(location =>  location.from.trim().replace(/\s/g, "").includes(sanitized))
+    this.setState({
+      temporaryResults: filteredByFromInput,
+      fromFilterTemp: fromInput,
+      toFilter: false
+    })
+  };
+
+  changeToFilter = (toInput) => {
+    var sanitized = toInput.toUpperCase().trim().replace(/\s/g, "")
+    const filteredByToInput = this.state.filteredResults.filter(location =>  location.to.trim().replace(/\s/g, "").includes(sanitized))
+    this.setState({
+      temporaryResults: filteredByToInput,
+      toFilterTemp: toInput
+    })
+  };
+
+  formSubmit = () => {
+    this.setState({
+      filteredResults: this.state.temporaryResults,
+      mainFilter: !!this.state.mainFilterTemp,
+      fromFilter: !!this.state.fromFilterTemp,
+      toFilter: !!this.state.toFilterTemp,
+      temporaryResults: []
+    })
   }
 
 
   render() {
     return (
       <div>
-        <button onClick={this.test}>Import</button>
+        <button onClick={this.getLocations}>Import Locations</button>
+        <button onClick={this.getSigns}>Import Signs</button>
+        <Filter 
+        
+        temporaryResults={this.state.temporaryResults} 
+        filteredResults={this.state.filteredResults} 
+
+        formSubmit={this.formSubmit} 
+
+        changeBorough={this.changeBorough} 
+        changeMainFilter={this.changeMainFilter} 
+        changeFromFilter={this.changeFromFilter} 
+        changeToFilter={this.changeToFilter}
+
+       
+        boroughFilter={this.state.boroughFilter} 
+        mainFilter={this.state.mainFilter}
+        fromFilter={this.state.fromFilter}
+        toFilter={this.state.toFilter}
+
+        mainFilterTemp={this.state.mainFilterTemp} 
+        fromFilterTemp={this.state.fromFilterTemp}
+        toFilterTemp={this.state.toFilterTemp}
+
+        
+
+        />
     </div>
     );
   }
---
+++ src/components/Filter.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class Filter extends React.Component{
+
+	handleSelectChange = (event) => {
+		this.props.changeBorough(event.target.value)
+	};
+
+	handleMainChange = (event) => {
+		this.props.changeMainFilter(event.target.value)
+	};
+
+	handleFromChange = (event) => {
+		this.props.changeFromFilter(event.target.value)
+	};
+
+	handleToChange = (event) => {
+		this.props.changeToFilter(event.target.value)
+	}
+
+	handleFormSubmission = (event) => {
+		event.preventDefault()
+		this.props.formSubmit()
+	}
+
+	render(){
+
+		console.log("All Results: ",this.props.filteredResults)
+		console.log("Temporary: ",this.props.temporaryResults)
+		console.log(this.props)
+
+		const mainStreetInput = (<input type="text"  disabled={this.props.mainFilter} value={this.props.mainFilterTemp} onChange={this.handleMainChange} placeholder="Enter primary street..."/>)
+		const fromInput = (<input type="text" disabled={this.props.fromFilter} value={this.props.fromFilterTemp} onChange={this.handleFromChange} placeholder="Enter from street..."/>)
+		const toInput = (<input type="text" disabled={this.props.toFilter} value={this.props.toFilterTemp} onChange={this.handleToChange} placeholder="Enter to street..."/>)
+
+		return(
+			<div>
+				<select value={this.props.boroughFilter} onChange={this.handleSelectChange}>
+					<option value="B">Bronx</option>
+					<option value="K">Brooklyn</option>
+					<option value="M">Manhattan</option>
+					<option value="Q">Queens</option>
+					<option value="S">Staten Island</option>
+				</select>
+				<form onSubmit={this.handleFormSubmission}>
+					{this.props.boroughFilter ? mainStreetInput : null }
+					{this.props.mainFilter ? fromInput : null}
+					{this.props.fromFilter ? toInput : null}
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+
+
+		)
+	}
+}
+
+export default Filter
