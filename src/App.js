8c75d87f06185098707103fc141615f420ac5249
sbal13 <sbal13@gmail.com>
2017-09-01 14:17:06 -0400

    PapaParse working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Papa from 'papaparse'
+
 
 class App extends Component {
+
+  state = {}
+
+
+  test = ()=> {
+    Papa.parse("http://a841-dotweb01.nyc.gov/datafeeds/ParkingReg/locations.CSV", {
+      download: true,
+      complete: function(results) {
+        console.log(results.data[0]);
+      }
+    });
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <button onClick={this.test}>Import</button>
+    </div>
     );
   }
 }
